a9891b53b5e9fefbc59d3ce941cc41690dbc2ded
Anish Asokan <CORRA\corra-151@corra-151.corra.loc>
2025-03-16 21:32:54 +0530

    dms update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -80,6 +80,7 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
+        {to: '/distributions', label: 'Distributions', position: 'left'},
         {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/corradevops/corradevops.github.io',
